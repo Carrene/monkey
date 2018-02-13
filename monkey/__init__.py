c6056600907d004fbbe889a640efcd74bd55b401
pylover <vahid.mardani@gmail.com>
2018-02-14 00:24:39 +0330

    Empty bilerplate

---
+++ monkey/__init__.py
@@ -0,0 +1,7 @@
+
+
+__version__ = '0.1.0'
+
+
+def main():
+    return 0
---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import re
+
+from setuptools import setup, find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'monkey', '__init__.py')) as v_file:
+    package_version = re.compile(r".*__version__ = '(.*?)'", re.S).match(v_file.read()).group(1)
+
+dependencies = [
+    'khayyam',
+    'pymqi',
+    'pymlconf'
+]
+
+
+setup(
+    name="monkey",
+    version=package_version,
+    author="Vahid Mardani",
+    author_email="vahid.mardani@gmail.com",
+    description="Scalable IBM Websphere MQ multi-thread scheduler.",
+    packages=find_packages(),
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': ['monkey = monkey:main']
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Intended Audience :: Developers',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
