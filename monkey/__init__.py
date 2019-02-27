9e4743808b1f9f186bb2687efbddb960b107838e
mehdi <45841074+pynuxer@users.noreply.github.com>
2019-02-27 06:46:17 -0500

    Change cli interface. add listen and pop subcommand (#4)

--- monkey/__init__.py
+++ monkey/__init__.py
@@ -9,36 +9,3 @@ from .configuration import settings, configure
 
 
 __version__ = '1.0.0'
-
-# TODO: Rename to listen and move to another module named listen.py
-#def main():
-#    if args.version:
-#        print(__version__)
-#        return
-#    configure()
-#    from .job import Job
-#    thread_pool = [threading.Thread(target=Job.worker, name='Worker%d' % i) for i in range(1, settings.threads+1)]
-#
-#    # Make sure the socket does not already exist
-#    try:
-#        os.unlink(settings.socket_file)
-#    except OSError:
-#        if os.path.exists(settings.socket_file):
-#            print('The socket file is already exists: %s' % settings.socket_file, file=sys.stderr)
-#
-#    # Create a UDS socket
-#    unix_socket = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-#    print('Listening on %s' % settings.socket_file)
-#    unix_socket.bind(settings.socket_file)
-#    os.chmod(settings.socket_file, stat.S_IRWXU + stat.S_IRWXG + stat.S_IROTH)
-#    unix_socket.listen(settings.backlog)
-#
-#    for thread in thread_pool:
-#        thread.daemon = True
-#        thread.start()
-#
-#    while True:
-#        # Wait for a connection
-#        print('Waiting for a connection')
-#        connection, client_address = unix_socket.accept()
-#        Job.put(Job(connection, client_address))
